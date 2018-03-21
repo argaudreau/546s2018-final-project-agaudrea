3a4557fe098f115d9f05ac78f6d577fdd3d2b804
Adam Gaudreau <adam@Adams-MacBook-Air.local>
2018-03-21 19:18:36 -0400

    init

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Week 1
+let btnReport1 = document.getElementById('button-report-1'),
+  btnManual1 = document.getElementById('button-manual-1'),
+  btnView1 = document.getElementById('button-view-1');
+btnReport1.addEventListener('click', function() {
+  window.location = "/~agaudrea/427546s2018/final-project/1/report-1.txt";
+});
+btnManual1.addEventListener('click', function() {
+  window.location = "/~agaudrea/427546s2018/final-project/1/manual-1.txt";
+});
+btnView1.addEventListener('click', function() {
+  window.location = "/~agaudrea/427546s2018/final-project/1/index.html";
+});
